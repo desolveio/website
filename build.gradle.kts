cc792c2889f011ae595d6b166f516edadcfe8d9a
Xavier <tehneon1212@gmail.com>
2022-05-31 15:47:39 -0400

    Force IntelliJ to download sources & javadocs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,2 +1,18 @@
 group = "io.desolve.website"
 version = "0.0.1"
+
+plugins {
+	idea
+}
+
+subprojects {
+	apply(plugin = "idea")
+
+	idea {
+		module {
+			isDownloadJavadoc = true
+			isDownloadSources = true
+		}
+	}
+
+}
