c326afd078998776f7e7d40abf85edcd4b48bb0f
Xavier <tehneon1212@gmail.com>
2022-05-31 16:54:01 -0400

    Relocated authentication testing to page1 for now

--- frontend/src/routes/page1.tsx
+++ frontend/src/routes/page1.tsx
@@ -1,8 +1,19 @@
+import AuthenticationAPI from "../api/AuthenticationAPI";
+
 export default function Page1() {
     return (
         <div>
             <h1>Page 1</h1>
             <p>Page one contents are soooo cool!</p>
+
+            <button onClick={ () => {
+                AuthenticationAPI.testAuthentication()
+                    .then(test => {
+                        console.log(`Test result = ${test.data}`)
+                    }, error => {
+                        console.log(`Test error = ${error}`)
+                    })
+            }}>Test Optional Auth</button>
         </div>
     )
 }
