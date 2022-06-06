1d5feada74b999a76eef05e138d5b7cfcc75cb7e
GrowlyX <growlyxdev@gmail.com>
2022-06-05 21:41:57 -0400

    Messing around with states

--- frontend/src/routes/page1.tsx
+++ frontend/src/routes/page1.tsx
@@ -1,6 +1,9 @@
 import AuthenticationAPI from "../api/AuthenticationAPI";
+import {useState} from "react";
 
 export default function Page1() {
+    const [status, setStatus] = useState("not called")
+
     return (
         <div>
             <h1>Page 1</h1>
@@ -10,10 +13,14 @@ export default function Page1() {
                 AuthenticationAPI.testAuthentication()
                     .then(test => {
                         console.log(`Test result = ${test.data}`)
+                        setStatus(test.data.toString())
                     }, error => {
                         console.log(`Test error = ${error}`)
+                        setStatus(error.toString())
                     })
             }}>Test Optional Auth</button>
+
+            <p>Status: {status}</p>
         </div>
     )
 }
